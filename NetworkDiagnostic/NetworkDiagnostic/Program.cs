104e900d1fb82129a6e3c1e1faaa346db79bcff1
Tymoshchuk Maksym <MTimoshchukmn.ua@gmail.com>
2023-09-08 19:12:48 +0300

    added style crop

---
+++ NetworkDiagnostic/NetworkDiagnostic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NetworkDiagnostic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
